aa7fcde139be00088cb284929113d0fe6b21c098
Andrew Lowndes <andrew.lowndes@gmail.com>
2023-01-14 22:12:14 +0000

    Add rust docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,50 @@
+//! Composable structs - reuse struct fields across multiple structs in code by storing the source of structs across macros.
+//! 
+//! # Usage
+//! 1. Add this macro to your project
+//! 
+//! 2. Add a `#[reusable(name)]` attribute to the structs you want to able to copy, where the name is a globally unique identifier for the struct (best to namespace using your crate and module names)
+//! 
+//! 3. Add a `#[reuse(name)]` attribute to the structs you want to reuse using the same name used in the attribute above
+//! 
+//! # Example
+//! ```ignore
+//! use reusable::{reusable, reuse};
+//! 
+//! #[reusable(test_name)]
+//! #[derive(Debug)]
+//! struct Name {
+//!     firstname: String,
+//!     surname: String,
+//! }
+//! 
+//! #[reuse(test_name)]
+//! #[derive(Debug)]
+//! struct Fullname {
+//!     middlename: String,
+//! }
+//! 
+//! fn main() {
+//!     let example = Fullname {
+//!         firstname: "Bob".to_string(),
+//!         middlename: "Frank".to_string(),
+//!         surname: "Junior".to_string(),
+//!     };
+//!     dbg!(example);
+//! }
+//! ```
+//! 
+//! # How it works
+//! 0. This crate relies heavily on [macro_state](https://crates.io/crates/macro_state) to share data between macro calls.
+//! 
+//! 1. The `reusable` attribute copies the tokenstream of a struct to a global state using the provided name as a key
+//! 
+//! 2. The `reuse` attribute reads the tokenstream set by `reusable`, parses the structs matching the names given and then appends the fields to the generated struct (any fields with the same name are skipped so can be overriden)
+//! 
+//! Note: multiple names can be provided to the `reuse` attribute, e.g. `#[reuse(name1, name2)]`.
+//! 
+//! Works in stable Rust, no nightly required.
+
 use macro_state::{proc_read_state, proc_write_state};
 use proc_macro::TokenStream;
 use quote::ToTokens;
